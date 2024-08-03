4fd14924b4094d4f1a1f7069f0945a294b10a146
ElrohirGT <elrohirgt@gmail.com>
2024-08-03 13:04:09 -0600

    feat: Added hornystein package

--- flake.nix
+++ flake.nix
@@ -66,16 +66,35 @@
           path = ./Hornystein/maze;
           name = "mazeFile";
         };
-        assets = builtins.path {
-          path = ./Hornystein/night_assets/.;
-          name = "horny-stein_assets";
+        # assets = builtins.path {
+        #   path = ./Hornystein/night_assets/.;
+        #   name = "horny-stein_assets";
+        # };
+
+        assets = pkgs.stdenv.mkDerivation {
+          # name of our derivation
+          name = "assets_dir";
+
+          # sources that will be used for our derivation.
+          src = ./Hornystein/night_assets/.;
+
+          # see https://nixos.org/nixpkgs/manual/#ssec-install-phase
+          # $src is defined as the location of our `src` attribute above
+          installPhase = ''
+            # $out is an automatically generated filepath by nix,
+            # but it's up to you to make it what you need. We'll create a directory at
+            # that filepath, then copy our sources into it.
+            mkdir $out
+            cp -rv $src/* $out
+          '';
         };
       in {
         hornystein = pkgs.writeShellApplication {
           name = "hornystein";
           runtimeInputs = [hornysteinPkg];
           text = ''
-            hornystein-bin ${mazeFile} ${assets}
+            # hornystein-bin ${mazeFile} ${assets}
+            hornystein-bin ${assets}
           '';
         };
       };
